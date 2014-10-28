e7bfb6d14d7fe86863908ccafc279eb0f23ad3e2
Chris Pavlina <cpavlin1@binghamton.edu>
2014-10-28 12:12:36 -0400

    Added schematic render

--- pcb/scripts/postprocess.sh
+++ pcb/scripts/postprocess.sh
@@ -3,3 +3,6 @@
 bash scripts/rename_gerbers.sh
 bash scripts/gerb_render.sh
 
+ps2pdf -sPAPERSIZE=legal renders/ad9958-dev.ps renders/ad9958-dev.pdf
+convert -alpha Off +antialias -density 40 renders/ad9958-dev.pdf renders/ad9958-dev_small.png
+
