24465e4c7df4b4ad3be985d9d76b5f59a73c0e5f
Chris Pavlina <cpavlin1@binghamton.edu>
2014-11-11 15:30:02 -0500

    Amended postprocessing script - PDF size

--- pcb/scripts/postprocess.sh
+++ pcb/scripts/postprocess.sh
@@ -8,15 +8,4 @@ setopt extended_glob
 
 ./scripts/gerb_render.sh $PROJECT
 
-pushd renders
-    for i in *.ps; do
-        ps2pdf -sPAPERSIZE=letter "$i" "${i%.ps}.pdf"
-    done
-
-    pdfunite $PROJECT.pdf *.pdf~$PROJECT.pdf~schematic.pdf(N) schematic.pdf
-
-    convert -alpha Off +antialias -density 40 $PROJECT.pdf schematic_small.png
-    rm *.pdf~schematic.pdf
-popd
-
-
+./scripts/convert_schematic.sh $PROJECT
